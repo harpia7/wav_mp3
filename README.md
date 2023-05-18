af176f73d69b0274bfd2d3a6acbfb28cdaac8466
harpia7 <harpia7@hotmail.com>
2023-05-18 15:51:46 -0300

    importacao original

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # wav_mp3
-
-primeira alteração do windows
-segunda alteração do windows
-
-primeira alteracao com linux/ssh
+#
+#			Script montado para trancodificar audio padrão wav para mp3 por lote 
+#		fazendo uso do ffmpeg via linha de comando
+#
+#  arquivo original criado em 03/2015
 
 bash script convert wav to mp3 !!!
+
+uso :
+	- copie para uma pasta todos os .wav que deseja transcodificar
+
+	- copie para dentro desta pasta wav_to_mp3.sh e certifique que tempermisao de execução rwx
+
+	- parametro opcional após o comando a taxa de bits por segundo deve ser escrita sem abreviaçoes
+		192k ==>> 192000
+		256k ==>> 256000
+		128k ==>> 128000 ( default )
+	
+	
+		ex: $ ./wav_to_mp3.sh 128000 
+
+			será gerado um arquivo temporário com lista de todas musicas .wav
+			
+			serão todos transcodificados para mp3 com bitrate pedido
+			
+			será criada uma subpasta wav onde serão movidos todos os arquivos originais
+			
+			temporarios serão apagados ! 
+
+
+
---
+++ wav_to_mp3.sh
@@ -0,0 +1,89 @@
+#! /bin/bash
+#
+#
+#		Script para codificar uma pasta com arquivos wav em mp3 
+#
+#
+#		Flavio	07/03/15
+#
+#
+
+declare -rx SCRIPT=${0##*/}
+declare -r T0="lista.txt"
+declare -r TMP="tmp.tmp"
+declare -i TOTAL
+declare MUS
+declare TRA
+declare -i TOTLET
+declare -i N
+declare AB
+
+#				Teste de sanidade
+# 	verifica parametros
+if [ $# -eq 0 ] ; then
+		printf "%s \n\n" "***********************************************************************"
+		printf "%s \n\n" "  USANDO bitrate de 128k !!! "
+		printf "%s \n\n" "***********************************************************************"
+		((AB=128000))
+	else
+		((AB="$1"))     # 	carrega o parametro
+fi
+
+#	abre o arquivo temporario para leitura escrita
+	exec 4> $T0
+
+# 	lista diretorio e filtra as .wav escreve em temporario e fecha
+	ls -1 *.wav >&4-
+
+#   conta total
+	((TOTAL=` cat $T0 | wc -l `))
+
+#	abre para leitura
+	exec 5< $T0
+
+#	looping de execução de musica
+	((N=0))
+
+	while  ((N++ < $TOTAL)) ; do 
+# 	le linha inteira
+			read -i text MUS <&5
+		
+		
+# 	calcula novo nome
+		# conta letras
+			((TOTLET=${#MUS}))				
+
+		# novo nome
+			exec 6> $TMP
+			printf "%s%s\n" "${MUS:0:TOTLET-3}" "mp3" >&6-
+		# rele novo nome
+			exec 6< $TMP
+			read -i text TRA <&6-
+		 
+		# executa comando conversâo e copia metadados
+		#	`ffmpeg -i "$MUS" -y -ab $AB -map_meta_data outfile:infile rrr.mp3`
+			`ffmpeg -i "$MUS" -ab $AB rrr.mp3`
+		# renomeia
+			`mv rrr.mp3 "$TRA"`	
+
+	done
+#	cria diretorio para remoção
+	`mkdir wav`
+#	move todos m4a para dentro 
+	`mv *.wav wav/. `
+#	move o script tambem
+	`mv $SCRIPT wav/. `
+#	remove temporarios	
+	`rm $T0`
+	`rm $TMP`
+	
+	printf "%s \n\n" "***********************************************************************"
+	printf "%s \n\n" " Arquivos recodificados !!!"
+	printf "%s \n\n" "***********************************************************************"
+exit 0
+
+
+
+
+
+

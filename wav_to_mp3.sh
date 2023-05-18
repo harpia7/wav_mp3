58d2979557cc47abbe9c236bc9e7b0599a5b7b99
harpia7 <harpia7@hotmail.com>
2023-05-18 18:21:33 -0300

    atualização do script

--- wav_to_mp3.sh
+++ wav_to_mp3.sh
@@ -5,7 +5,7 @@
 #
 #
 #		Flavio	07/03/15
-#
+#				18/05/23
 #
 
 declare -rx SCRIPT=${0##*/}
@@ -18,68 +18,77 @@ declare -i TOTLET
 declare -i N
 declare AB
 
-#				Teste de sanidade
 # 	verifica parametros
 if [ $# -eq 0 ] ; then
-		printf "%s \n\n" "***********************************************************************"
-		printf "%s \n\n" "  USANDO bitrate de 128k !!! "
-		printf "%s \n\n" "***********************************************************************"
+
 		((AB=128000))
 	else
 		((AB="$1"))     # 	carrega o parametro
 fi
 
+printf "%s \n\n" "***********************************************************************"
+printf "%s %s  !!! \n\n" "  USANDO bitrate de " $AB
+printf "%s \n\n" "***********************************************************************"
+
+
 #	abre o arquivo temporario para leitura escrita
-	exec 4> $T0
+exec 4> $T0
 
 # 	lista diretorio e filtra as .wav escreve em temporario e fecha
-	ls -1 *.wav >&4-
+ls -1 *.wav >&4-
 
 #   conta total
-	((TOTAL=` cat $T0 | wc -l `))
+((TOTAL=` cat $T0 | wc -l `))
 
 #	abre para leitura
-	exec 5< $T0
+exec 5< $T0
 
 #	looping de execução de musica
-	((N=0))
+((N=0))
 
-	while  ((N++ < $TOTAL)) ; do 
-# 	le linha inteira
-			read -i text MUS <&5
+while  ((N++ < $TOTAL)) ; 
+do 
+	# 	le linha inteira
+	read -i text MUS <&5
 		
-		
-# 	calcula novo nome
-		# conta letras
-			((TOTLET=${#MUS}))				
+	# 	calcula novo nome
+	# conta letras
+	((TOTLET=${#MUS}))				
 
-		# novo nome
-			exec 6> $TMP
-			printf "%s%s\n" "${MUS:0:TOTLET-3}" "mp3" >&6-
-		# rele novo nome
-			exec 6< $TMP
-			read -i text TRA <&6-
+	# novo nome
+	exec 6> $TMP
+	printf "%s%s\n" "${MUS:0:TOTLET-3}" "mp3" >&6-
+
+	# rele novo nome
+	exec 6< $TMP
+	read -i text TRA <&6-
 		 
-		# executa comando conversâo e copia metadados
-		#	`ffmpeg -i "$MUS" -y -ab $AB -map_meta_data outfile:infile rrr.mp3`
-			`ffmpeg -i "$MUS" -ab $AB rrr.mp3`
-		# renomeia
-			`mv rrr.mp3 "$TRA"`	
+	# executa comando conversâo e copia metadados
+	#	`ffmpeg -i "$MUS" -y -ab $AB -map_meta_data outfile:infile rrr.mp3`
+	`ffmpeg -i "$MUS" -ab $AB rrr.mp3`
 
-	done
+	# renomeia
+	`mv rrr.mp3 "$TRA"`	
+
+done
+
 #	cria diretorio para remoção
-	`mkdir wav`
+`mkdir wav`
+
 #	move todos m4a para dentro 
-	`mv *.wav wav/. `
+`mv *.wav wav/. `
+
 #	move o script tambem
-	`mv $SCRIPT wav/. `
+`mv $SCRIPT wav/. `
+
 #	remove temporarios	
-	`rm $T0`
-	`rm $TMP`
+`rm $T0`
+`rm $TMP`
 	
-	printf "%s \n\n" "***********************************************************************"
-	printf "%s \n\n" " Arquivos recodificados !!!"
-	printf "%s \n\n" "***********************************************************************"
+printf "%s \n\n" "***********************************************************************"
+printf "%s \n\n" " Arquivos recodificados !!!"
+printf "%s \n\n" "***********************************************************************"
+
 exit 0
 
 
